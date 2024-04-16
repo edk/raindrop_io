40fdfc9c27ef90f9da99c6ecbf1661fcef0bea6b
Ed Kim <54091+edk@users.noreply.github.com>
2024-04-15 18:33:19 -0700

    Initial commit

---
+++ lib/raindrop_io.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "raindrop_io/version"
+
+module RaindropIo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "raindrop_io"
+
+require "minitest/autorun"
