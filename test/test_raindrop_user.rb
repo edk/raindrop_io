002b7c664522f1154ae5a6859736503b395606cd
Ed Kim <54091+edk@users.noreply.github.com>
2024-04-24 07:43:34 -0700

    some test fixes based up changes to raindrop models

--- test/test_raindrop_user.rb
+++ test/test_raindrop_user.rb
@@ -15,7 +15,7 @@ class TestRaindropIoRaindrop < Minitest::Test
       refute_nil user.is_a?(RaindropIo::ApiError)
       assert user.fullName == "user123"
       assert user.name == "user123"
-      assert user.id.to_s == "123456789", "Expected 123456789, got #{user.id}"
+      assert user._id.to_s == "123456789", "Expected 123456789, got #{user._id}"
 
       # Per the api docs, nested collections are to be found in the user record under groups
       assert user.groups.size == 1, "Expected 1 group, got #{user.groups.size}"
@@ -30,12 +30,12 @@ class TestRaindropIoRaindrop < Minitest::Test
       assert user.fullName == "user123", "Expected user123, got #{user.fullName}"
       assert user.name == "user123", "Expected user123, got #{user.name}"
       assert user.email == "user123@example.com", "Expected email: user123@example.com, got #{user.email}"
-      assert user.id.to_s == "123456789", "Expected 123456789, got #{user.id}"
+      assert user._id.to_s == "123456789", "Expected 123456789, got #{user._id}"
       assert user.groups.size == 1, "Expected 1 group, got #{user.groups.size}"
       assert user.groups[0]["collections"] == [43358979, 43330903, 36370053, 43358978], "Expected collections, got #{user.groups[0]["collections"]}"
       collections = user.collections
       assert collections.size == 4, "Expected 4 collections, got #{collections.size}"
-      assert_equal collections[0].id, "123456789"
+      assert_equal collections[0]._id, "123456789"
       assert_equal collections[1].title, "tech"
     end
   end
